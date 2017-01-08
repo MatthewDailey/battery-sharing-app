fee69e5d98cb28714483037cc1145011fc5e7f2f
Matthew Dailey <matthew.j.dailey@gmail.com>
2017-01-08 13:22:05 -0800

    enable showing battery % on app

--- BatteryShare/index.common.js
+++ BatteryShare/index.common.js
@@ -11,25 +11,56 @@ import {
 
 import { RNBatteryStatus } from 'NativeModules';
 
-RNBatteryStatus.batteryStatus(
-  "getLevel", // getLevel, turnOff
-
-  function errorCallback(results) {
-    console.log('JS Error: ' + results['errMsg']);
-  },
-
-  function successCallback(results) {
-    console.log('JS Success: ' + results['successMsg']);
-  }
-);
-
+function parseBatteryLevel(successMsg) {
+  return parseFloat(
+    successMsg.replace('Battery level is ', '').replace(' percent', '')).toFixed(2);
+}
 
 class RCTBattery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      batteryLevel: undefined,
+      errorMessage: undefined,
+    }
+  }
+
+  componentDidMount() {
+    console.log('Component did mount.');
+
+    const setBatteryLevel = (batteryLevel, errorMessage) => {
+      console.log('Called setBatteryLevel ', batteryLevel, errorMessage);
+      this.setState({
+        batteryLevel,
+        errorMessage
+      }, () => console.log(this.state));
+    };
+
+    RNBatteryStatus.batteryStatus(
+      "getLevel", // getLevel, turnOff
+      function errorCallback(results) {
+        setBatteryLevel(undefined);
+        console.log('JS Error: ', results);
+      },
+      function successCallback(results) {
+        setBatteryLevel(parseBatteryLevel(results.successMsg));
+        console.log('JS Success: ', results);
+      }
+    );
+  }
+
   render() {
+    console.log('Render.');
+
+    const batteryPercentString = this.state.batteryLevel
+      ? `${this.state.batteryLevel}%`
+      : `Unknown. ${this.state.errorMessage}`;
+
     return (
       <View style={styles.container}>
         <Text>
-          Hello
+          Battery Level: {batteryPercentString}
         </Text>
       </View>
     );
