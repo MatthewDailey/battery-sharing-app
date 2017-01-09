9187b8d1ac86806915e95e4300ce43703a77fa77
Daniel Bercht <d.bercht@gmail.com>
2017-01-08 17:13:11 -0800

    add background task notifier

--- BatteryShare/index.common.js
+++ BatteryShare/index.common.js
@@ -8,6 +8,9 @@ import {
 } from 'react-native';
 
 require('./firebaseConnector');
+import { RNBatteryStatus } from 'NativeModules';
+import BackgroundFetch from "react-native-background-fetch";
+
 
 let styles = StyleSheet.create({
   container: {
@@ -20,6 +23,25 @@ let styles = StyleSheet.create({
 });
 
 class RCTBattery extends Component {
+  backgroundCallback() {
+    console.log("[app] Registering background callback.");
+    BackgroundFetch.configure({
+      stopOnTerminate: false
+    }, function() {
+      console.log("[js] Received background-fetch event");
+
+      // To signal completion of your task to iOS, you must call #finish!
+      // If you fail to do this, iOS can kill your app.
+      BackgroundFetch.finish();
+    }, function(error) {
+      console.log("[js] RNBackgroundFetch failed to start");
+    });
+  }
+
+  componentDidMount() {
+    this.backgroundCallback()
+  }
+
   render() {
     console.log('Render.');
 
@@ -34,4 +56,3 @@ class RCTBattery extends Component {
 }
 
 AppRegistry.registerComponent('BatteryShare', () => RCTBattery);
-
